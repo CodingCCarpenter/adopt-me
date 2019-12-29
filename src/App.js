af876401305aa6872229bcc49f0e82b2831a4c9c
CodingCCarpenter <christineassists@aol.com>
2019-12-29 15:44:03 -0600

    adding props to Pet component to make name, breed, and animal dynamic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+const Pet = (props) => {
+    return React.createElement('div', {}, [
+        React.createElement('h1', {}, props.name),
+        React.createElement('h2', {}, props.animal),
+        React.createElement('h2', {}, props.breed),      
+    ]);
+};
+
 const App = () => {
     return React.createElement(
         "div",
         {},
-        React.createElement("h1", {}, "Adopt Me!")
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet, { name:"Shy", animal:"Dog", breed:'Austrian Shepard'}),
+            React.createElement(Pet, { name:"Patches", animal:"Cat", breed:'Calico'}),
+            React.createElement(Pet, { name:"Meescha", animal:"Dog", breed:'Siberian Husky'})
+        ]
     );
-    };
+};
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
