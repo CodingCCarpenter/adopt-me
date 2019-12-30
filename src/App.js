173f56ef2c721bf9c64a3d823a2ee56619699a51
CodingCCarpenter <christineassists@aol.com>
2019-12-29 21:39:29 -0600

    add prettierrc and eslintrc.json

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 const Pet = ({ name, animal, breed }) => {
-    return React.createElement('div', {}, [
-        React.createElement('h1', {}, name),
-        React.createElement('h2', {}, animal),
-        React.createElement('h2', {}, breed),      
-    ]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
 };
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, { name:"Shy", animal:"Dog", breed:'Austrian Shepard'}),
-            React.createElement(Pet, { name:"Patches", animal:"Cat", breed:'Calico'}),
-            React.createElement(Pet, { name:"Meescha", animal:"Dog", breed:'Siberian Husky'})
-        ]
-    );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Shy",
+      animal: "Dog",
+      breed: "Austrian Shepard"
+    }),
+    React.createElement(Pet, {
+      name: "Patches",
+      animal: "Cat",
+      breed: "Calico"
+    }),
+    React.createElement(Pet, {
+      name: "Meescha",
+      animal: "Dog",
+      breed: "Siberian Husky"
+    })
+  ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
